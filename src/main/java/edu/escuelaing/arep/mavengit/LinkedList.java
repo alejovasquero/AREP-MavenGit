3a62496727ab631fcf76f2942b10c7c3cf1d4da8
alejovasquero <alejo_704@hotmail.com>
2020-08-09 00:38:08 -0500

    First commit :)

---
+++ src/main/java/edu/escuelaing/arep/mavengit/LinkedList.java
@@ -0,0 +1,19 @@
+package edu.escuelaing.arep.mavengit;
+
+public class LinkedList extends Collections{
+    
+
+    
+    
+
+    
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public
+    
+}
